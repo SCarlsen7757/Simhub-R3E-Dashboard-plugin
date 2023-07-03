02981bf5cab34b0e6969b9b5ae533f3286c34de6
Mark Carlsen <mark@scarlsen.dk>
2023-07-03 18:14:51 +0200

    Add project files.

---
+++ SimhubR3ETyreAndBrakeColorPlugin.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simhub_R3E_Tyre_and_brake_color_plugin
+{
+    public class SimhubR3ETyreAndBrakeColorPlugin
+    {
+    }
+}
