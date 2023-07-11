4d2dd3640a01b1f97fdea73c0a8845f6beccb308
Mark Carlsen <mark@scarlsen.dk>
2023-07-11 16:30:42 +0200

    Removed unused using

--- Math/LinearFunction.cs
+++ Math/LinearFunction.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Numerics;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Numerics;
 
 namespace Simhub_R3E_Dashboard_plugin.Math
 {
