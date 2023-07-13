0f31b1eef60678d71848ebe2db451e824cda28c3
Mark Carlsen <mark@scarlsen.dk>
2023-07-13 16:09:15 +0200

    Added unit test for UpdatedTemperatureSettings

--- Simhub-R3E-Dashboard-pluginTests/Models/Temperature/R3ETemperatureColorTests.cs
+++ Simhub-R3E-Dashboard-pluginTests/Models/Temperature/R3ETemperatureColorTests.cs
@@ -24,7 +24,7 @@ namespace Simhub_R3E_Dashboard_plugin.Model.Tests
 
             //Cold test
             temperature = 0;
-             color = R3ETemperatureColor.ColorConverter(temperature, optimal, min, max, colorSettings);
+            color = R3ETemperatureColor.ColorConverter(temperature, optimal, min, max, colorSettings);
             Assert.AreEqual<string>("#00FFFF", color);
 
             //Semi cold test
@@ -38,7 +38,7 @@ namespace Simhub_R3E_Dashboard_plugin.Model.Tests
             Assert.AreEqual<string>("#00FF00", color);
             //Optimal test
             temperature = 100;
-             color = R3ETemperatureColor.ColorConverter(temperature, optimal, min, max, colorSettings);
+            color = R3ETemperatureColor.ColorConverter(temperature, optimal, min, max, colorSettings);
             Assert.AreEqual<string>("#00FF00", color);
             //Upper optimal test
             temperature = 110;
@@ -52,8 +52,48 @@ namespace Simhub_R3E_Dashboard_plugin.Model.Tests
 
             //Hot test
             temperature = 200;
-             color = R3ETemperatureColor.ColorConverter(temperature, optimal, min, max, colorSettings);
+            color = R3ETemperatureColor.ColorConverter(temperature, optimal, min, max, colorSettings);
             Assert.AreEqual<string>("#FF0000", color);
+        }
+
+        [TestMethod()]
+        public void UpdatedTemperatureSettingsTest()
+        {
+            R3ETemperatureColor temperatureColor = new R3ETemperatureColor();
+            OptimalTemperatureColorSettings.TemperatureValues temperatureValues;
+            double optimalTemperature;
+
+            //Test 1 with Absolute values
+            temperatureValues = new OptimalTemperatureColorSettings.TemperatureValues();
+            temperatureValues.Min.Absolute = 0;
+            temperatureValues.Max.Absolute = 200;
+            temperatureValues.Range.Lower = 10;
+            temperatureValues.Range.Upper = 10;
+
+            optimalTemperature = 100;
+
+            temperatureColor.UpdatedTemperatureSettings(optimalTemperature, temperatureValues);
+            Assert.AreEqual<double>(0, temperatureColor.Min);
+            Assert.AreEqual<double>(200, temperatureColor.Max);
+            Assert.AreEqual<double>(90, temperatureColor.Optimal.Range.Lower);
+            Assert.AreEqual<double>(110, temperatureColor.Optimal.Range.Upper);
+            Assert.AreEqual<double>(100, temperatureColor.Optimal.Value);
+
+            //Test 2 with relative values
+            temperatureValues = new OptimalTemperatureColorSettings.TemperatureValues();
+            temperatureValues.Min.Relative = 50;
+            temperatureValues.Max.Relative = 100;
+            temperatureValues.Range.Lower = 50;
+            temperatureValues.Range.Upper = 30;
+
+            optimalTemperature = 500;
+
+            temperatureColor.UpdatedTemperatureSettings(optimalTemperature, temperatureValues);
+            Assert.AreEqual<double>(400, temperatureColor.Min);
+            Assert.AreEqual<double>(630, temperatureColor.Max);
+            Assert.AreEqual<double>(450, temperatureColor.Optimal.Range.Lower);
+            Assert.AreEqual<double>(530, temperatureColor.Optimal.Range.Upper);
+            Assert.AreEqual<double>(500, temperatureColor.Optimal.Value);
         }
     }
 }
