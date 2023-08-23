e17c79d3190109f094bf6c218705150fffc365a6
Mark Carlsen <mark@scarlsen.dk>
2023-08-23 14:44:00 +0200

    Updated test, so it can compile.

--- Simhub-R3E-Extra-properties-pluginTests/Models/Sector/SectorColorTests.cs
+++ Simhub-R3E-Extra-properties-pluginTests/Models/Sector/SectorColorTests.cs
@@ -25,25 +25,25 @@ namespace Simhub_R3E_Extra_properties_plugin.Models.Sector.Tests
             SectorColorSettings.Colors colors = new SectorColorSettings.Colors(notRun, slow, personalBest, overallBest);
 
             //Test with no new/current sector time
-            time = new Sector.SectorTime(null, 300, 100, 200);
+            time = new Sector.SectorTime(null, new TimeSpan(300), new TimeSpan(100), new TimeSpan(200));
 
             color = SectorColor.ColorConverter(colors, time);
             Assert.AreEqual<HEX>(new HEX("111312"), color);
 
             //Test with slow new/current sector time
-            time = new Sector.SectorTime(250, 300, 100, 200);
+            time = new Sector.SectorTime(new TimeSpan(250), new TimeSpan(300), new TimeSpan(100), new TimeSpan(200));
 
             color = SectorColor.ColorConverter(colors, time);
             Assert.AreEqual<HEX>(new HEX("FFD400"), color);
 
             //Test with personal best new/current sector time
-            time = new Sector.SectorTime(150, 300, 100, 200);
+            time = new Sector.SectorTime(new TimeSpan(150), new TimeSpan(300), new TimeSpan(100), new TimeSpan(200));
 
             color = SectorColor.ColorConverter(colors, time);
             Assert.AreEqual<HEX>(new HEX("3FD93F"), color);
 
             //Test with overall best new/current sector time
-            time = new Sector.SectorTime(50, 300, 100, 200);
+            time = new Sector.SectorTime(new TimeSpan(50), new TimeSpan(300), new TimeSpan(100), new TimeSpan(200));
 
             color = SectorColor.ColorConverter(colors, time);
             Assert.AreEqual<HEX>(new HEX("B028D2"), color);
